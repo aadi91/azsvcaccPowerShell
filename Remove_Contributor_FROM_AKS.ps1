a0357dcd7cc0e6ba414381af9c73737476207ca4
azsvcacc <106141299+azsvcacc@users.noreply.github.com>
2022-09-09 13:20:39 +0530

    Update Remove_Contributor_FROM_AKS.ps1

--- Remove_Contributor_FROM_AKS.ps1
+++ Remove_Contributor_FROM_AKS.ps1
@@ -17,17 +17,18 @@
     $aksID = $_.Id
     
     #Get-AzRoleAssignment -Scope "$aksID" | Where{($_.RoleDefinitionName -eq 'Contributor') -or ($_.RoleDefinitionName -like 'WBA Contributor' )} | ForEach-Object{
-    Get-AzRoleAssignment -Scope "$aksID" | Where{($_.RoleDefinitionName -eq 'Azure Kubernetes Service Cluster Admin Role')} | ForEach-Object{
+    Get-AzRoleAssignment -Scope "$aksID" | Where{($_.RoleDefinitionName -eq 'Azure Kubernetes Service Cluster Admin Role') -or ($_.RoleDefinitionName -like 'Azure Kubernetes Service RBAC Admin' )} -WarningAction SilentlyContinue | ForEach-Object{
     $UserName = $_.DisplayName
     $ROlesDefination = $_.RoleDefinitionName
     $ObjectID = $_.ObjectId
     $SignInName = $_.SignInName
-    }
+   
     Remove-AzRoleAssignment -ObjectId "$ObjectID" -RoleDefinitionName "$ROlesDefination" -Scope "$aksID"
 
+    
 
 
-        
+         }
    
         }
        } 
