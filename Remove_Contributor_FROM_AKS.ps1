91be280631405c580aba4f57048f55ea4d49fdfd
azsvcacc <106141299+azsvcacc@users.noreply.github.com>
2022-09-09 13:15:03 +0530

    Create Remove_Contributor_FROM_AKS.ps1

---
+++ Remove_Contributor_FROM_AKS.ps1
@@ -0,0 +1,36 @@
+    connect-azAccount
+    Get-AzSubscription | ForEach-Object{
+    $subscriptionId = $_.Id
+    $subscriptionName = $_.Name
+    $subscriptionTags = $_.Tags
+    $TenentID = $_.TenantId
+    $tagcount = $_.Tags.Count
+    Set-AzContext -SubscriptionId $subscriptionId
+
+    Get-AzResourceGroup | ForEach-Object{
+    $resourceGroupName = $_.ResourceGroupName
+
+    Get-AzAksCluster -ResourceGroupName $resourceGroupName -WarningAction SilentlyContinue | ForEach-Object{
+
+    $aksName = $_.Name
+    $aksFDQN = $_.Fqdn
+    $aksID = $_.Id
+    
+    #Get-AzRoleAssignment -Scope "$aksID" | Where{($_.RoleDefinitionName -eq 'Contributor') -or ($_.RoleDefinitionName -like 'WBA Contributor' )} | ForEach-Object{
+    Get-AzRoleAssignment -Scope "$aksID" | Where{($_.RoleDefinitionName -eq 'Azure Kubernetes Service Cluster Admin Role')} | ForEach-Object{
+    $UserName = $_.DisplayName
+    $ROlesDefination = $_.RoleDefinitionName
+    $ObjectID = $_.ObjectId
+    $SignInName = $_.SignInName
+    }
+    Remove-AzRoleAssignment -ObjectId "$ObjectID" -RoleDefinitionName "$ROlesDefination" -Scope "$aksID"
+
+
+
+        
+   
+        }
+       } 
+      }
+ 
+Write-Host "script executed successfully" -ForegroundColor Green
